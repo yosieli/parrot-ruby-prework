50b9197009272d547493310223c63b2d811fe09f
feven <feventsegay6@gmail.com>
2019-04-15 23:09:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name="Squawk!")
-   name
+def parrot(phrase="Squawk!")
+    puts phrase
+  return phrase
    
 end
